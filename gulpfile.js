9fe178b2599780cbf4fd69ae80de21fc59bb0b96
yzzting <yzz1995@aliyun.com>
2016-04-13 10:21:20 +0800

    fix float bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp')
 var less = require('gulp-less')
 var cssmin = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 gulp.task('default',function () {
     gulp.src(['source/less/main.less'])
         .pipe(less())
         .pipe(cssmin())
+        .pipe(postcss([autoprefixer({
+            browsers: ['last 2 versions']
+        })]))
         .pipe(gulp.dest('source/css'));
 });
 gulp.task('lessWatch',function () {
