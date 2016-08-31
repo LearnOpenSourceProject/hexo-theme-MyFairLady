d45097f58e0ee8543a8e49213ae0ed41f5a21827
yzzting <yzz1995@aliyun.com>
2016-08-31 22:59:40 +0800

    delete less version use scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var cssmin = require('gulp-minify-css');
-var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
+var postcss = require('gulp-postcss');
 
-gulp.task('less', function() {
-    gulp.src(['source/less/main.less'])
-        .pipe(less())
+gulp.task('sass', function() {
+    return gulp.src('./source/sass/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
         .pipe(cssmin())
         .pipe(postcss([autoprefixer({
             browsers: ['last 2 versions']
         })]))
-        .pipe(gulp.dest('./source/css'));
+        .pipe(gulp.dest('./source/dist'));
 });
-gulp.task('default', ['less'], function() {
-    gulp.watch('source/less/*.less', ['less'])
-    gulp.watch('source/less/_partial/*.less', ['less'])
-    gulp.watch('source/less/_partial/post/*.less', ['less'])
-})
+
+gulp.task('sass-lint', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError());
+});
+
+gulp.task('watch', ['sass'], function() {
+    gulp.watch('./source/sass/**/*.scss', ['sass']);
+    gulp.watch('./source/sass/**/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'sass-lint', 'watch']);
